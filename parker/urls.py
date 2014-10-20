7e90c7d9041bc355ccab512e0c20c9911bdde825
Anden sin limite <root@andensinlimite.org>
2014-10-20 17:42:01 +0100

    Moved site to /parker/

--- parker/urls.py
+++ parker/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^parker/admin/', include(admin.site.urls)),
 
-    url(r'^', include('registro.urls')),
+    url(r'^parker/', include('registro.urls')),
 )
