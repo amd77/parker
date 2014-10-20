4040d469b5985f069f8191ed1df42d97025d3dc0
Anden sin limite <root@andensinlimite.org>
2014-10-20 18:29:45 +0100

    Puestas rutas absolutas para el sqlite

--- parker/settings.py
+++ parker/settings.py
@@ -4,6 +4,11 @@ DEBUG = True
 #DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -13,7 +18,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
