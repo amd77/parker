311435afd1916ad07d497ee1e344d2867aeb1139
Alberto Morales <amd77@galotecnia.com>
2015-08-16 18:09:52 +0100

    Agregada inicio sesion y cambio clave

--- parker/settings.py
+++ parker/settings.py
@@ -54,18 +54,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(BASE_DIR, '.media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/parker/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '.static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '.static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -166,8 +166,8 @@ LOGGING = {
     }
 }
 
-LOGIN_URL="/parker/login"
-LOGOUT_URL="/parker/logout"
+LOGIN_URL="/parker/accounts/login"
+LOGOUT_URL="/parker/accounts/logout"
 LOGIN_REDIRECT_URL="/parker/"
 
 import socket
--- parker/urls.py
+++ parker/urls.py
@@ -1,20 +1,24 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'parker.views.home', name='home'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^', include('empresa.urls')),
+    url(r'^', include('inventario.urls')),
+    url(r'^', include('tickets.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^admin/', include(admin.site.urls)),
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^parker/', include('empresa.urls')),
-    url(r'^parker/', include('inventario.urls')),
-    url(r'^parker/', include('tickets.urls')),
-    url(r'^parker/admin/', include(admin.site.urls)),
-    url(r'^parker/matriculas/', include('matriculas.urls')),
+    url(r'^matriculas/', include('matriculas.urls')),
 ]
+
+urlpatterns = [
+    url(r'^parker/', include(urlpatterns)),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
